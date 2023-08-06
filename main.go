6d383e18f72d41cbf0b5d5c3d77e798140c5add8
Anali <b.anali.silva@gmail.com>
2023-08-06 19:42:04 -0300

    Iniciando um projeto que simula algumas movimentações bancárias

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 	if podeSacar {
 		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
+		return "Saque realizado com sucesso."
 	} else {
-		return "Saldo insuficiente"
+		return "Saldo insuficiente."
 	}
 }
 
